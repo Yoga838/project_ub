5fc7592bd73a70d7e7976c3b77cf6c63720f2924
Ahmad Rizky Febianto <ahmadrizky13jkl@gmail.com>
2025-05-15 02:38:33 +0700

    update(component) : navbar & footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-import Navbar from './component/navbar';
-import Footer from './component/footer';
+import Navbar from './component/landing/navbar';
+import Footer from './component/landing/footer';
 
 function App() {
   const [count, setCount] = useState(0);
