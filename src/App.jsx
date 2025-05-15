77711069fe361035be850283496c1721ca0e9701
Ahmad Rizky Febianto <ahmadrizky13jkl@gmail.com>
2025-05-15 15:14:02 +0700

    feat (pages) : rekam jejak, publikasi, pemateri ipc, ketpen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Test from "./pages/index";
+import RekamJejak from "./pages/about/rekamJejak/index";
+import Publikasi from "./pages/about/rekamJejak/publikasi/index";
+import Pemateri from "./pages/about/rekamJejak/pemateri/index";
+import Ketpen from "./pages/about/rekamJejak/ketpen/index";
 import Dashboard from "./pages/dashboard";
 import PagesHome from "./pages/dashboard/pagesHome";
 import PagesWork from "./pages/dashboard/pagesWork";
@@ -9,7 +13,16 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Rute utama */}
         <Route path="/" element={<Test />} />
+        
+        {/* Rute Rekam Jejak */}
+        <Route path="/Tentang Saya/Rekam Jejak" element={<RekamJejak />} />
+        <Route path="/Tentang Saya/Rekam Jejak/Publikasi" element={<Publikasi />} />
+        <Route path="/Tentang Saya/Rekam Jejak/Pemateri" element={<Pemateri />} />
+        <Route path="/Tentang Saya/Rekam Jejak/Ketpen" element={<Ketpen />} />
+
+        {/* Rute Dashboard dengan rute anak */}
         <Route path="/dashboard" element={<Dashboard />}>
           <Route index element={<PagesHome />} />
           <Route path="programkerja" element={<PagesWork />} />
