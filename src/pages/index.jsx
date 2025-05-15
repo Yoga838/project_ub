81e21ce97877c0932683ff861cfb5ffd692fc486
RendyRensFirmansyah <rendyfirmansyah.job@gmail.com>
2025-05-15 10:08:25 +0700

    move branch develop

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import Sidebar from "../component/dashboard/sidebar";
 import Navbar from "../component/landing/navbar";
- dashboard
-=======
+
 import Footer from "../component/landing/footer";
- main
 
 export default function Test() {
   return (
     <>
- dashboard
       <Navbar />
-=======
-    <Navbar/>
- main
+
       <h1 className="font-bold text-7xl">Hello World</h1>
-      <Footer/>
+      <Footer />
     </>
   );
 }
