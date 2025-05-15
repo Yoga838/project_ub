d06abc77d58ba6c268a804fea944293686c50d37
RendyRensFirmansyah <rendyfirmansyah.job@gmail.com>
2025-05-15 14:24:41 +0700

    feat: index pendidikan & visi misi

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Test from "./pages/index";
 import Dashboard from "./pages/dashboard";
 import PagesHome from "./pages/dashboard/pagesHome";
 import PagesWork from "./pages/dashboard/pagesWork";
+import Pendidikan from "./pages/about/pendidikan";
+import VisiMisi from "./pages/about/visiMisi";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Test />} />
+        <Route path="/Tentang Saya/Pendidikan" element={<Pendidikan />} />
+        <Route path="/Tentang Saya/Visi Misi" element={<VisiMisi />} />
         <Route path="/dashboard" element={<Dashboard />}>
           <Route index element={<PagesHome />} />
           <Route path="programkerja" element={<PagesWork />} />
