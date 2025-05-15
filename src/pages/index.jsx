a5f49d7fdba22786508de65000dc5a9cd8133335
Ahmad Rizky Febianto <ahmadrizky13jkl@gmail.com>
2025-05-15 07:56:15 +0700

    update landing pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Sidebar from "../component/dashboard/sidebar";
+import Navbar from "../component/landing/navbar";
+import Footer from "../component/landing/footer";
 
 export default function Test() {
   return (
     <>
+    <Navbar/>
       <h1 className="font-bold text-7xl">Hello World</h1>
+      <Footer/>
     </>
   );
 }
